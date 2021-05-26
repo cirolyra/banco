e22c5adf274a0de69caa386474baef751146afde
cirol <cirol@Lenovo>
2021-05-26 19:30:07 -0300

    versão 3

--- src/application/conta.java
+++ src/application/conta.java
@@ -11,44 +11,42 @@ public class conta {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		DadosBancarios dadosBancarios = new DadosBancarios();
+
+		DadosBancarios dadosBancarios;
 
 		System.out.print("Enter account number: ");
 		int account = sc.nextInt();
 		sc.nextLine();
 		System.out.print("Enter account holder: ");
-		String name = sc.nextLine();
+		String holder = sc.nextLine();
 		System.out.print("Is there an initial deposit (y/n):");
 		char resp = sc.next().charAt(0);
 
 		if (resp != 'y') {
-			System.out.println();
-			dadosBancarios = new DadosBancarios(account, name);
+			dadosBancarios = new DadosBancarios(account, holder);
 		} else {
 			System.out.println();
 			System.out.println("Enter initial deposit value: ");
-			double value = sc.nextDouble();
-			dadosBancarios = new DadosBancarios(account, name, value);
+			double initialValue = sc.nextDouble();
+			dadosBancarios = new DadosBancarios(account, holder, initialValue);
 		}
-		
+
 		System.out.println("Account data: ");
 		System.out.println(dadosBancarios);
-		
+
 		System.out.println();
 		System.out.print("Enter a deposit value: ");
-		double value = sc.nextDouble();
-		dadosBancarios.deposit(value);
+		double depositValue = sc.nextDouble();
+		dadosBancarios.deposit(depositValue);
 		System.out.println("Updated account data: ");
 		System.out.println(dadosBancarios);
-		
+
 		System.out.println();
 		System.out.print("Enter a withdraw value: ");
-		value = sc.nextDouble();
-		dadosBancarios.withdraw(value);
+		double withdrawValue = sc.nextDouble();
+		dadosBancarios.withdraw(withdrawValue);
 		System.out.println("Updated account data: ");
 		System.out.println(dadosBancarios);
-		
 
 		sc.close();
 
